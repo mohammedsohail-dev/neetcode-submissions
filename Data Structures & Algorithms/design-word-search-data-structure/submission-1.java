94d6f821e4a7d42c6c667732ab421f127c1cb04c
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2026-04-06 04:17:49 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.java
@@ -0,0 +1,33 @@
+class WordDictionary {
+    private List<String> store;
+
+    public WordDictionary() {
+        store = new ArrayList<>();
+    }
+
+    public void addWord(String word) {
+        store.add(word);
+    }
+
+    public boolean search(String word) {
+        for(String w : store){
+            if(w.length() == word.length()){
+                int i = 0;
+                while(i < w.length()){
+                    if(w.charAt(i) == word.charAt(i) || word.charAt(i) == '.'){
+                        i++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if ( i == w.length() ){
+                    return true;
+                }
+
+                
+            }
+        }
+        return false;
+    }
+}
