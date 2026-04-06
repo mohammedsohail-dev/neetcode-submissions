a9fb974167b875f624589670a8ae93d6e081399b
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2026-04-06 11:49:13 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = nums.length-1;
+        for(int i = nums.length-2;i>=0;i--){
+            if(i+nums[i]>=reachable){
+                reachable = i;
+            }
+        }
+        
+        return reachable == 0;
+    }
+}
+
