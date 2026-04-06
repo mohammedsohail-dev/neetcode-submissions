595c0e2baf93bf04e3e1a30cbc58b1d3f77151d4
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2026-04-06 11:49:19 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int farthest = 0;
+        int jump = 0;
+        int currentEnd = 0;
+        for(int i=0;i<nums.length-1;i++){
+            farthest = Math.max(farthest, nums[i]+i);
+
+            if(i==currentEnd){
+                jump++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jump;
+    }
+}
